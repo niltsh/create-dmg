bcc8eb289892b60043dedda8e8fba4b8be4b65f7
Zongyao QU <zongyao.qu@gmail.com>
2018-07-16 22:34:49 +0900

    update for isntaller 1.1.4

--- make-dmg.sh
+++ make-dmg.sh
@@ -4,22 +4,23 @@ WORKDIR=MPlayerX
 
 if [[ -e ${WORKDIR}/MPlayerX.app ]]; then
 
-	echo "===============re-codesign the installer==============="
-	codesign --force -s "Developer ID Application: Zongyao Qu" -v ${WORKDIR}/MPlayerX.app/Contents/bin/AssetsChanger
-	echo ""
+#	echo "===============re-codesign the installer==============="
+#	codesign --force -s "Developer ID Application: Zongyao Qu" -v ${WORKDIR}/MPlayerX.app/Contents/bin/AssetsChanger
+#	echo ""
 	codesign --force -s "Developer ID Application: Zongyao Qu" -v ${WORKDIR}/MPlayerX.app
 	echo ""
 
 	echo "===============check the result==============="
-	codesign -dvvv ${WORKDIR}/MPlayerX.app/Contents/bin/AssetsChanger
-	echo
+#	codesign -dvvv ${WORKDIR}/MPlayerX.app/Contents/bin/AssetsChanger
+#	echo
 	codesign -dvvv ${WORKDIR}/MPlayerX.app
-	echo
+	echo ""
+	echo ""
 	spctl --verbose=4 --assess --type execute ${WORKDIR}/MPlayerX.app
 	echo ""
 
 	echo "===============Make the DMG==============="
-	./create-dmg --volname MPlayerX --background resources/dmg_background.jpg  --window-size 548 455 --icon-size 128 --icon MPlayerX 274 227 MPlayerX.dmg ${WORKDIR}/	
+	./create-dmg --volname MPlayerX --background resources/dmg_background.jpg  --window-size 548 432 --icon-size 160 --icon MPlayerX 274 216 MPlayerX.dmg ${WORKDIR}/	
 else 
 	echo "installer app does not exist."
 	exit 1
